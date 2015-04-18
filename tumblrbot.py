b765a5ed936a90d082a65fb7ad0c6f2ebb84d1c9
thedeadparrot <thedeadparrot@gmail.com>
2015-04-18 19:08:22 -0400

    Up the word count of Tumblrbot.

--- tumblrbot.py
+++ tumblrbot.py
@@ -5,7 +5,7 @@ import pytumblr
 import util
 from generator import generate_text
 
-text = generate_text(num_words=300)
+text = generate_text(num_words=500)
 oauth_config = util.load_oauth_config('tumblr')
 blog_name = util.load_blog_name()
 client = pytumblr.TumblrRestClient(*oauth_config)
